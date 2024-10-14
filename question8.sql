005f1e4a6fa874c805c8b7620865318f186d2cd2
Student <jsantoscustodio@appdev.yearup.org>
2024-10-14 17:00:19 -0500

    adding question 8

--- question8.sql
+++ question8.sql
@@ -1,8 +1,19 @@
 ## Use JOIN to display the total amount rung up by each staff member in August of 2005, using tables staff and payment ## 
-##COME BACK TO THIS ONE## 
-SELECT staff.first_name, staff. last_name, payment.payment_date, .staff_id, (amount) AS 'sum(pay.amount)'
-FROM payment
-JOIN staff 
-ON payment.staff_id = staff.staff_id
-GROUP BY staff_id
-HAVING payment_date =  2005-08-01;
+## Staff on the left ##
+## payment on the right ## 
+
+SELECT first_name, last_name, SUM(amount) AS 'sum(pay.amount)' 
+FROM staff
+JOIN payment ON payment.staff_id = staff.staff_id
+WHERE payment_date BETWEEN '2005-08-01 00:00:00' AND '2005-08-31 23:59:59' 
+GROUP BY first_name, last_name;
+
+
+
+
+
+
+
+
+
+
