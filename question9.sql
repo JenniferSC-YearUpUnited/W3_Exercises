41a450911b6e33b1a6d1e03061bfc37b0ea50c7f
Student <jsantoscustodio@appdev.yearup.org>
2024-10-14 00:10:09 -0500

    adding answer to question 9

--- question9.sql
+++ question9.sql
@@ -1,3 +1,12 @@
+## List each film and the number of actors who are listed for that film. Use tables film_actor and film. Use inner join## 
+
 ## film on the left##
 ## film_actor on the right## 
 ##COMEBACK TO THIS ONE ## 
+SELECT film.title, COUNT(actor_id) AS 'number_of_actors'
+FROM film_actor
+INNER JOIN film ON film.film_id = film_actor.film_id
+GROUP BY title
+HAVING COUNT(actor_id) >= 12
+ORDER BY COUNT(actor_id) DESC;
+
