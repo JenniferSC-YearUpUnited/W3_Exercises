4c67b3986039c0f594b8d90c3745e0d1c5612809
Student <jsantoscustodio@appdev.yearup.org>
2024-10-13 22:26:40 -0500

    adding query

--- question10.sql
+++ question10.sql
@@ -1,12 +1,9 @@
  ## How many copies of the film HUNCHBACK IMPOSSIBLE exist in the system? ## 
-SELECT title 
-FROM film
-WHERE title = "HUNCHBACK IMPOSSIBLE"
-UNION
-SELECT COUNT(film_id) AS 'Number_in_inventory'
+SELECT title, COUNT(title) AS 'number_in_inventory'
 FROM inventory
-WHERE film_id = 6
-
+JOIN film ON film.film_id=inventory.film_id
+GROUP BY title
+HAVING title = "HUNCHBACK IMPOSSIBLE" AND COUNT(title) = "6";
 
 
 
