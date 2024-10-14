06ba30637cbececb315396ee8a67d1d1b805a6cd
Student <jsantoscustodio@appdev.yearup.org>
2024-10-14 17:51:24 -0500

    updating q4 and q5

--- 2C_query_practice.sql
+++ 2C_query_practice.sql
@@ -5,28 +5,25 @@ FROM northwind.products
 ORDER BY UnitPrice ASC;
 
 ## What are the products that we carry where we have at least 100 units on hand? Order them in descending order by price ##
-
+## No two or more have the same unit price on the output results ## 
 SELECT ProductName, UnitPrice
 FROM northwind.products
 WHERE UnitsInStock >= 100
 ORDER BY UnitPrice DESC;
 
-## No two or more have the same unit price on the output results ## 
-
-
 #### Write a query against the orders table that displays the total number of distinct customers who have placed orders by customerid ## 
-##NEED HELP ###
 
-
+SELECT CustomerID, COUNT(DISTINCT OrderID) AS 'totalorders'
+FROM orders
+GROUP BY CustomerID; 
 
 #### Write a query against the orders table that displays the total number of distinct  customers who have placed orders, by customer ID, for each country where orders have been shipped ##
-###NEEDHELP## 
+SELECT * FROM orders; 
+SELECT CustomerID, COUNT(DISTINCT OrderID) AS 'totalorders', ShipCountry
+FROM orders
+GROUP BY CustomerID, ShipCountry; 
 
-
-
-
-
- ## What are the products that we carry where we have no units on hand, but 1 or more units of them are on backorder? Order them by product name ## 
+## What are the products that we carry where we have no units on hand, but 1 or more units of them are on backorder? Order them by product name ## 
  
 SELECT ProductName, UnitsInStock, UnitsOnOrder
 FROM northwind.products
